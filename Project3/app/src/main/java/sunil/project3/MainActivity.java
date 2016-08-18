4fe6430a506f8b656582f40d61a5de19bbd05b29
Anders Palm <ang.palm@hotmail.com>
2016-08-18 16:03:31 -0400

    finished with toggleable gallery

--- Project3/app/src/main/java/sunil/project3/MainActivity.java
+++ Project3/app/src/main/java/sunil/project3/MainActivity.java
@@ -1,16 +1,35 @@
 package sunil.project3;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -24,12 +43,18 @@ import sunil.project3.CardObjects.NasaObj;
 import sunil.project3.CardObjects.TwitterObj;
 
 public class MainActivity extends AppCompatActivity {
+//public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     public RecyclerView mRecyclerView;
     public RecyclerView mRecyclerView2;
+    public ImageView mImageView1, mImageView2, mImageView3, mImageView4;
+    HorizontalScrollView mHorizontalScrollView;
+    Button mToggle;
 
     CursorAdapter mCursorAdapter;
-    TextView mT1, mT2, mT3, mT4, mT5;
+//    TextView mT1, mT2, mT3, mT4, mT5;
     ListView mListView;
+    CardView mHorizontalCardView;
+
     private static final String TAG = "MainActivity";
 
     @Override
@@ -37,15 +62,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mListView = (ListView) findViewById(R.id.HorizontalIMGlistView);
+        mToggle = (Button) findViewById(R.id.toggle);
+        mHorizontalCardView = (CardView) findViewById(R.id.horizontal_scrollview);
+
+        String marsUrl = "http://highmars.org/wp-content/uploads/2016/05/high-mars-10.jpg";
+        String eartUrl = "https://lifesjourneyblog.files.wordpress.com/2013/07/planet-earth-from-space.jpg";
+        String person1 = "http://www.tvchoicemagazine.co.uk/sites/default/files/imagecache/interview_image/intex/michael_emerson.png";
+        String person2 = "https://d.ibtimes.co.uk/en/full/1356835/number-2-u-s-president-barack-obama-second-most-admired-person-planet.jpg?w=400";
+
+
+        ArrayList<String> scrollViewURLS = new ArrayList<String>();
+        scrollViewURLS.add(eartUrl);
+        scrollViewURLS.add(marsUrl);
+        scrollViewURLS.add(person1);
+        scrollViewURLS.add(person2);
+
+        ImagScrollViewAdapter ImgAdapter = new ImagScrollViewAdapter(this, scrollViewURLS);
+        mListView.setAdapter(ImgAdapter);
+        mToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int height = mListView.getHeight();
+                if (height < 100) {
+                    mHorizontalCardView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 800));
+                    Log.i(TAG, "onClick: height " + height);
+                }
+                else {
+                    mHorizontalCardView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0));
+                    Log.i(TAG, "onClick: height " + height);
+                }
+            }
+        });
+
+
         String temp1 = "From which we spring! Drake Equation, kindling the energy hidden in matter Drake Equation Euclid.";
         String temp2 = "Great turbulent clouds at the edge of forever consectetur star stuff harvesting star ligh";
         String temp3 = "White dwarf Euclid paroxysm of global death of brilliant syntheses concept of the number oneinteriors of collapsing stars";
         String temp4 = "Vanquish the impossible the carbon in our apple pies hydrogen atoms globular star cluster gr star light.";
         String temp5 = "Apollonius of Perga? Citizens of distant epochs? At the edge of forever colonies a very smal hydrogen atoms colonies";
-        String marsUrl = "http://highmars.org/wp-content/uploads/2016/05/high-mars-10.jpg";
-        String eartUrl = "https://lifesjourneyblog.files.wordpress.com/2013/07/planet-earth-from-space.jpg";
-        String person1 = "http://www.tvchoicemagazine.co.uk/sites/default/files/imagecache/interview_image/intex/michael_emerson.png";
-        String person2 = "https://d.ibtimes.co.uk/en/full/1356835/number-2-u-s-president-barack-obama-second-most-admired-person-planet.jpg?w=400";
 
         TwitterObj twitterObj1 = new TwitterObj(person1, "name", temp5, "8/14/2016", "Anders");
         TwitterObj twitterObj2 = new TwitterObj(person2, "name", temp3, "8/14/2016", "Anders");
@@ -55,6 +110,18 @@ public class MainActivity extends AppCompatActivity {
         NYTObj nytObj2 = new NYTObj(temp1, temp2, temp3, temp4);
         NasaObj nasaObj1 = new NasaObj(marsUrl, "An article on Mars", "because");
         NasaObj nasaObj2 = new NasaObj(eartUrl, "An article on Earth", "because");
+
+
+
+//
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.setDrawerListener(toggle);
+//        toggle.syncState();
+//
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
 
 
         List<CardObject> masterList = CardObjSingleton.getInstance().getMasterList();
@@ -131,4 +198,61 @@ public class MainActivity extends AppCompatActivity {
 //        astroINList.add("Vanquish the impossthe. ");
 //        astroINList.add("Apollonius of Perga? Citizens .");
     }
+
+//    @Override
+//    public void onBackPressed() {
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        if (drawer.isDrawerOpen(GravityCompat.START)) {
+//            drawer.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main2, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    @SuppressWarnings("StatementWithEmptyBody")
+//    @Override
+//    public boolean onNavigationItemSelected(MenuItem item) {
+//        // Handle navigation view item clicks here.
+//        int id = item.getItemId();
+//
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
+//
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        drawer.closeDrawer(GravityCompat.START);
+//        return true;
+//    }
 }
